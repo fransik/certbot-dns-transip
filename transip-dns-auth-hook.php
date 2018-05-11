3565c97a9d441a8beb85da87012600a99749dec4
Frans Heeremans <mail@fransik.nl>
2018-05-11 21:07:17 +0100

    create and deploy the challenge record

--- transip-dns-auth-hook.php
+++ transip-dns-auth-hook.php
@@ -2,15 +2,18 @@
 <?php
 require_once('Transip/DomainService.php');
 
+const TTL = 60;
+const CHALLENGE_NAME = '_acme-challenge';
+
 $domain = getenv('CERTBOT_DOMAIN');
-$validation = getenv('CERTBOT_VALIDATION');
+$token = getenv('CERTBOT_VALIDATION');
 $baseDomain = findBaseDomain($domain);
 $dnsEntries = getDnsEntries($baseDomain);
+deployChallenge($baseDomain, $dnsEntries, $token);
 
 echo 'DOMAIN: ' . $domain, PHP_EOL;
-echo 'VALIDATION: ' . $validation, PHP_EOL;
+echo 'TOKEN: ' . $token, PHP_EOL;
 echo 'BASE DOMAIN: ' . $baseDomain, PHP_EOL;
-print_r($dnsEntries);
 
 function getDomainNames()
 {
@@ -35,10 +38,10 @@ function findBaseDomain($domain)
 	$domains = getDomainNames();
 	$domainParts = explode('.', $domain);
 
-	foreach ($domainParts as $part)
+	foreach($domainParts as $part)
 	{
 		$search = implode('.', $domainParts);
-		if (in_array($search, $domains, TRUE))
+		if(in_array($search, $domains, TRUE))
 		{
 			$baseDomain = $search;
 			break;
@@ -46,7 +49,7 @@ function findBaseDomain($domain)
 		array_shift($domainParts);
 	}
 
-	if (! $baseDomain)
+	if(!$baseDomain)
 	{
 		echo '[ERROR] Domain could not be found in TransIP account.' . PHP_EOL;
 		exit(1);
@@ -70,4 +73,43 @@ function getDnsEntries($domain)
 	}
 
 	return $dnsEntries;
+}
+
+function deployChallenge($domain, $dnsEntries, $token)
+{
+	$challengesFound = 0;
+
+	foreach($dnsEntries as $key => $dnsEntry)
+	{
+		// Challenge record already exists, overwrite it with the new challenge
+		if($dnsEntry->name === CHALLENGE_NAME)
+		{
+			$dnsEntries[$key] = new Transip_DnsEntry(CHALLENGE_NAME, TTL, Transip_DnsEntry::TYPE_TXT, $token);
+			$challengesFound++;
+		}
+	}
+
+	// Create a new challenge record
+	$dnsEntries[] = new Transip_DnsEntry(CHALLENGE_NAME, TTL, Transip_DnsEntry::TYPE_TXT, $token);
+	$challengesFound++;
+
+	if($challengesFound > 1)
+	{
+		echo '[ERROR] More than one challenge record found.' . PHP_EOL;
+		exit(1);
+	}
+
+	print_r($dnsEntries);
+
+	try
+	{
+		// Commit the changes to TransIP's DNS service
+		Transip_DomainService::setDnsEntries($domain, $dnsEntries);
+		echo 'The DNS-01 challenge has been successfully deployed.';
+	}
+	catch(SoapFault $e)
+	{
+		echo '[ERROR] ' . $e->getMessage(), PHP_EOL;
+		exit(1);
+	}
 }
